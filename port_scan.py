503a091cdeb1804f011b08a16a1f9653a8891fd7
bobby <blahblah@gmail>
2025-01-24 18:47:38 +1100

    Added a theaded scan function and a sequential scan function

--- port_scan.py
+++ port_scan.py
@@ -1,9 +1,11 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
-PORT = 4444
+PORTS = list(range(200, 500))
 
-def test_port(host: str, port: int):
+
+def test_port(host: str, port: int) -> None:
     print(f'[*] Testing Port {port} on {host}...')
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -19,5 +21,23 @@ def test_port(host: str, port: int):
         return None
     
 
+def sequential_scan(ports: list[int]) -> None:
+    for port in ports:
+        test_port(port)
+
+
+def threaded_scan(ports: list[int]) -> None:
+    threads = []
+
+    for port in ports:
+        thread = threading.Thread(target=test_port, args=(port,))
+        thread.start()
+
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
 if __name__ == '__main__':
-    test_port(HOST, PORT)
+    for port in PORTS:
+        test_port(port)
