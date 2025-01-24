7f89db9dd6107b81f067f78a9c511a7c35eb9d62
bobby <blahblah@gmail>
2025-01-24 23:08:10 +1100

    Added tests for the port formatting function format_ports

--- test_port_scan.py
+++ test_port_scan.py
@@ -31,5 +31,46 @@ class TestPortValidation(unittest.TestCase):
         self.assertFalse(port_scan.validate_port(70000), 'Invalid Port: 70000')
 
 
+class TestFormatPorts(unittest.TestCase):
+
+    def test_valid_single_port(self):
+        self.assertEqual(port_scan.format_ports('40'), [40])
+        self.assertEqual(port_scan.format_ports('40000'), [40000])
+        self.assertEqual(port_scan.format_ports('1'), [1])
+        self.assertEqual(port_scan.format_ports('65535'), [65535])
+
+    def test_invalid_single_port(self):
+        self.assertRaises(ValueError, port_scan.format_ports('0'))
+        self.assertRaises(ValueError, port_scan.format_ports('-1'))
+        self.assertRaises(ValueError, port_scan.format_ports('65536'))
+        self.assertRaises(ValueError, port_scan.format_ports('abc'))
+
+    def test_valid_range_of_ports(self):
+        valid_inputs = ["40-2000", "1-65535", "1000-2000"]
+
+        for input in valid_inputs:
+            start = int(input.split('-')[0])
+            stop = int(input.split('-')[1]) + 1
+
+            self.assertEqual(port_scan.format_ports(input), [range(start, stop)])
+
+    def test_invalid_range_of_ports(self):
+        invalid_inputs = ["40-200000", "2000-40", "1--2000", "abc-2000", "40-def"]
+
+        for input in invalid_inputs:
+            self.assertRaises(ValueError, port_scan.format_ports(input))
+
+    def test_valid_list_of_ports(self):
+        self.assertEqual(port_scan.format_ports("21,22,80,139,443,445"), [21,22,80,139,443,445])
+        self.assertEqual(port_scan.format_ports("1,2,3,4,5"), [1,2,3,4,5])
+        self.assertEqual(port_scan.format_ports("1024,2048,4096"), [1024,2048,4096])
+
+    def test_invalid_list_of_ports(self):
+        self.assertRaises(ValueError, port_scan.format_ports("21,22,abc,80"))
+        self.assertRaises(ValueError, port_scan.format_ports("65536,22,80"))
+        self.assertRaises(ValueError, port_scan.format_ports("21,22,,80"))
+        self.assertRaises(ValueError, port_scan.format_ports("21;22;80"))
+
+
 if __name__ == '__main__':
     unittest.main()
