79763634849aa3b3b7f4592d33de181ba6d9ea35
bobby <blahblah@gmail>
2025-01-24 19:33:41 +1100

    Added port validation test

--- test_port_scan.py
+++ test_port_scan.py
@@ -8,13 +8,25 @@ class TestHostValidation(unittest.TestCase):
         valid_hosts = ['192.168.0.1', '127.0.0.1', '10.0.0.2', '172.16.254.1', '8.8.8.8', '255.255.255.255', '0.0.0.0']
 
         for host in valid_hosts:
-            self.assertTrue(port_scan.validate_host(host))
+            self.assertTrue(port_scan.validate_host(host), f'Valid Host: {host}')
 
     def test_invalid_hosts(self):
         invalid_hosts = ["256.256.256.256", "192.168.0.999", "192.168.1.", "192.168..1", "192.168.1.1.1", "192.168.1.-1", "192.168.01.1", "abc.def.ghi.jkl", "192.168.1.1/24" ]
 
         for host in invalid_hosts:
-            self.assertFalse(host)
+            self.assertFalse(port_scan.validate_host(host), f'Invalid Host: {host}')
+
+
+class TestPortValidation(unittest.TestCase):
+
+    def test_valid_ports(self):
+        for port in range(1, 65535 + 1):
+            self.assertTrue(port_scan.validate_port(port), f'Valid Port: {port}')
+
+    def test_invalid_ports(self):
+        self.assertFalse(port_scan.validate_port(-1), 'Invalid Port: -1')
+        self.assertFalse(port_scan.validate_port(0), 'Invalid Port: 0')
+
 
 if __name__ == '__main__':
     unittest.main()
