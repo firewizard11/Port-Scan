bf0c2671acc36b19b5817e9512bb6bd0462b7a03
bobby <blahblah@gmail>
2025-01-24 23:51:09 +1100

    Implemented format_ports: Method that tranforms a string rep of a port format into a list of ints (port numbers)

--- port_scan.py
+++ port_scan.py
@@ -80,6 +80,53 @@ def validate_host(host: str) -> bool:
     return True
 
 
+def format_ports(ports: str) -> list[int]:
+    """ Formats various forms of port inputs into a list of port numbers 
+    
+    Supported Formats:
+    - Single Port (e.g. "40" or "40000")
+    - Range of Ports (Inclusive) (e.g. "40-2000")
+    - List of Ports (e.g. 21,22,80,139,443,445)
+    """
+    # List of Ports 
+    if ',' in ports:
+        temp = ports.split(',')
+
+        for elem in temp:
+            if not elem.isdecimal():
+                raise ValueError(f'Please Enter a list of valid ports: {elem}')
+            
+            if not validate_port(int(elem)):
+                raise ValueError(f'Error: Invalid Port in List {elem}')
+            
+        return [int(port) for port in temp]
+    
+    if '-' in ports:
+        temp = ports.split('-')
+
+        if len(temp) != 2:
+            raise ValueError(f'Error: Incorrect Number of Ports in Range of Ports: {ports}')
+        
+        for elem in temp:
+            if not elem.isdecimal():
+                raise ValueError(f'Please Enter a Range of valid ports: {elem}')
+            
+            if not validate_port(int(elem)):
+                raise ValueError(f'Error: Invalid Port in Range {elem}')
+            
+        start = int(temp[0])
+        stop = int(temp[1]) + 1
+
+        return list(range(start, stop))
+
+    if not ports.isdecimal():
+        raise ValueError(f'Use a Supported Port Format: {ports}')
+    
+    if not validate_port(int(ports)):
+        raise ValueError(f'Enter a valid port: {ports}')
+    
+    return [int(ports)]
+
 if __name__ == '__main__':
     for port in PORTS:
         test_port(port)
